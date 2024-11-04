e34525b99fa714561a614ebb9b61567016f17d94
Sergey Shevchenko <gurucomkz@users.noreply.github.com>
2024-11-04 01:04:13 +0000

    fix devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-docker compose build
-docker compose up -d
+docker compose -f .devcontainer/docker-compose.yml up -d
 composer install
+
+export SS_DATABASE_CLASS=MySQLDatabase
+export SS_DATABASE_SERVER="localhost"
+export SS_DATABASE_USERNAME="root"
+export SS_DATABASE_PASSWORD="secret"
+export SS_DATABASE_CHOOSE_NAME=true
